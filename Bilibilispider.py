e2d9610df3b905bbb85e0d4d0819abb3e2f23c94
zmvg <aoilzmvg@gmail.com>
2022-04-14 07:55:17 +0800

    'added sys.argv[]'

--- Bilibilispider.py
+++ Bilibilispider.py
@@ -1,14 +1,16 @@
 import requests
 import json
 from bs4 import BeautifulSoup
+import sys
 
 #@auther : aoilzmvg
 #python爬虫练手
-headers1 = {
+
+headers1 = {   #  get html
     'user-agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.84 Safari/537.36'
 }
 
-headers2 = {
+headers2 = {   # get byte file
     'Range' : 'bytes=0-',
     'Referer' : 'https://www.bilibili.com/video/',
     'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.84 Safari/537.36'
@@ -23,7 +25,7 @@ def get_urls(bv):
     audio_urls = json_urls['data']['dash']['audio']
     return video_urls,audio_urls
 
-def check_url(video_urls,audio_urls):
+def check_url(video_urls,audio_urls):    #  pick up the urls
     urls = []
     video_url = video_urls[3]['baseUrl']
     audio_url = audio_urls[0]['baseUrl']
@@ -36,12 +38,15 @@ def download(filename,url,bv):
         sp1 = requests.get(url=url,headers=headers2)
         f.write(sp1.content)
 
-if __name__ == '__main__':
-    bv = input('请输入bv号：')
+def main():
+    bv = sys.argv[1]
     headers2['Referer'] = headers2['Referer'] + bv
     video_urls,audio_urls = get_urls(bv)
     urls = check_url(video_urls,audio_urls)
-    print('已获取视频地址')
-    print('正在下载视频')
+    print('[+] get urls')
+    print('[+] downloading video')
     download('video.m4s',urls[0],bv)
     download('audio.m4s',urls[1],bv)
+
+if __name__ == '__main__':
+    main()
---
+++ README.md
@@ -0,0 +1,10 @@
+Bilibilispider_test
+---------------------
+a test of Pyhton Spider
+
+using method:
+----------
+python Bilibilispider.py BV_code
+
+example:    
+    python Bilibilispider.py BVA1A1A1A
